9f1b16a3153d95d5ee063d4d37e1d7e3c6c381b9
Idan Asraf <idan.asraf@sap.com>
2017-09-12 13:18:34 +0300

    forgot await on table delete

--- test/assets/db-init.js
+++ test/assets/db-init.js
@@ -5,36 +5,39 @@ const Order = require("../../src/model/order");
 
 class DbInitializer {
     async init() {
-        try {
-            // Drop existing tables.
-            await Promise.all([
-                thinky.r.db("test").table("Customer").delete().run(),
-                thinky.r.db("test").table("Card").delete().run(),
-                thinky.r.db("test").table("Order").delete().run()
-            ]);
-        } finally {
-            // Re-create & populate tables.
-            this.customers = await this.createCustomers();
-            this.cards = await this.createCards();
-            this.orders = await this.createOrders();
-        }
+        // Re-create & populate tables.
+        this.customers = await this.createCustomers();
+        this.cards = await this.createCards();
+        this.orders = await this.createOrders();
 
         console.info("Populated database...");
     }
 
     async createCustomers() {
-        let mockCustomers = require("./mock-customers.json");
-        return await Promise.all(mockCustomers.map(c => Customer(c).save()));
+        try {
+            await thinky.r.db("test").table("Customer").delete().run();
+        } finally {
+            let mockCustomers = require("./mock-customers.json");
+            return await Promise.all(mockCustomers.map(c => Customer(c).save()));
+        }
     }
 
     async createCards() {
-        let mockCards = require("./mock-cards.json");
-        return await Promise.all(mockCards.map(c => Card(c).save()));
+        try {
+            await thinky.r.db("test").table("Card").delete().run();
+        } finally {
+            let mockCards = require("./mock-cards.json");
+            return await Promise.all(mockCards.map(c => Card(c).save()));
+        }
     }
 
     async createOrders() {
-        let mockOrders = require("./mock-orders.json");
-        return await Promise.all(mockOrders.map(o => Order(o).save()));
+        try {
+            await thinky.r.db("test").table("Order").delete().run();
+        } finally {
+            let mockOrders = require("./mock-orders.json");
+            return await Promise.all(mockOrders.map(o => Order(o).save()));
+        }
     }
 }
 
